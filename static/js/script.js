0094791ef684c83b577f3be81bdf856632456b71
MuzhdaN <muzhda.noorzad122@gmail.com>
2022-11-18 14:56:36 +0000

    Deleted contents from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -8,35 +8,3 @@ document.getElementById('pause').onclick = function() {
     $('#audio').html('');
 }
 
-var tooltipSpan = document.getElementById('details-box');
-var map = document.querySelectorAll('[data-name]');
-document.addEventListener('mouseover', function (e) {
-    
-    try {
-        if (e.target.tagName == 'path') {
-            if(map == undefined) {
-                console.log("coming soon");
-            }
-            var content = e.target.dataset.name;
-            document.getElementById("details-box").innerHTML = content;
-            document.getElementById("details-box").style.opacity = "100%";
-            map.style.opacity = "100%";
-            
-        }
-        else {
-            document.getElementById("details-box").style.opacity = "0%";
-            map.style.opacity = "0%";
-        }
-    } catch (error) {
-        console.log("error");
-        
-    }
-   
-});
-
-window.onmousemove = function (e) {
-    var x = e.clientX,
-        y = e.clientY;
-    tooltipSpan.style.top = (y + 50) + 'px';
-    tooltipSpan.style.left = (x) + 'px';
-};
