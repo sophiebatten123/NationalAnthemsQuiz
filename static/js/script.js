8d67bdd4529f2f2cd8c3b0dd4e7d40a8837e5dc4
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-11-18 16:29:04 +0000

    Adding country selection

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,27 @@
+// Round One
+
 document.getElementById('play').onclick = function() {
     document.getElementById('play').innerHTML = '<i class="fa-solid fa-rotate-right"></i>';
     $('#audio').html('<audio autoplay><source src="media/audio.mp3"></audio>');
 }
-
-
+    
 document.getElementById('pause').onclick = function() {
     $('#audio').html('');
 }
 
+let country = document.getElementsByClassName("country")
+
+for (let i=0; i < country.length; i++) {
+    country[i].addEventListener('click', function(e) {
+        if(country[i].id == 'Indonesia') {
+            alert('YAY');
+        } else {
+            alert('WRONG');
+        }
+    })
+}
+
+// Map Information
 var tooltipSpan = document.getElementById('details-box');
 var map = document.querySelectorAll('[data-name]');
 document.addEventListener('mouseover', function (e) {
@@ -15,7 +29,6 @@ document.addEventListener('mouseover', function (e) {
     try {
         if (e.target.tagName == 'path') {
             if(map == undefined) {
-                console.log("coming soon");
             }
             var content = e.target.dataset.name;
             document.getElementById("details-box").innerHTML = content;
@@ -27,9 +40,7 @@ document.addEventListener('mouseover', function (e) {
             document.getElementById("details-box").style.opacity = "0%";
             map.style.opacity = "0%";
         }
-    } catch (error) {
-        console.log("error");
-        
+    } catch (error) {  
     }
    
 });
