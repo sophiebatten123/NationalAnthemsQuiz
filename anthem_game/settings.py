a254becd5d6863d529758ab31ad2ff4dc1d5584c
LauraMayock <mcfadden.laura@yahoo.com>
2022-11-18 15:53:09 +0000

    Add proc file and secret key. attempt deployment

--- anthem_game/settings.py
+++ anthem_game/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r(ym*33*1d5na+b&(6x#%tqh_30r#!v(^@u!==s#+!fi+stlu1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anthemgame.herokuapp.com', 'localhost']
 
 
 # Application definition
