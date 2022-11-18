025e84e975a585fbcbbfdc8775e05a4a8841e6ef
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-11-18 18:14:07 +0000

    White noise removed from settings

--- anthem_game/settings.py
+++ anthem_game/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'anthem_game.urls'
