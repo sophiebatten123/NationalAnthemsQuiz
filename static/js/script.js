1dc4cee7a5d767600f7b545315505b3fb2a0998f
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-11-18 23:26:09 +0000

    Add quiz questions

--- static/js/script.js
+++ static/js/script.js
@@ -82,20 +82,47 @@ function roundTwo() {
     }
 }
 
-document.getElementById('pause').onclick = function() {
-    $('#audio').html('');
-}
+// Round Three
 
-let country = document.getElementsByClassName("country")
+function roundThree() {
+    document.getElementById('round').innerHTML = "<p>Round Three</p>";
+    document.getElementById('China').style.fill = "blue";
+    document.getElementById('Canada').style.fill = "green";
+    document.getElementById('pop-up').style.display = "none";
+    document.getElementById('play').innerHTML = '<i class="fa-solid fa-circle-play"></i>';
 
-for (let i=0; i < country.length; i++) {
-    country[i].addEventListener('click', function(e) {
-        if(country[i].id == 'Indonesia') {
-            alert('YAY');
-        } else {
-            alert('WRONG');
-        }
-    })
+    document.getElementById('play').onclick = function() {
+        document.getElementById('play').innerHTML = '<i class="fa-solid fa-rotate-right"></i>';
+        $('#audio').html('<audio autoplay><source src="media/france.mp3"></audio>');
+    }
+        
+    document.getElementById('pause').onclick = function() {
+        $('#audio').html('');
+    }
+    
+    let country = document.getElementsByClassName("country");
+    
+    for (let i=0; i < country.length; i++) {
+        country[i].addEventListener('click', function(e) {
+            if(country[i].id == 'France') {
+                document.getElementById('France').style.fill = "black";
+                document.getElementById('pop-up-text').innerHTML = "<p>Correct Answer</p>";
+                document.getElementById('pop-up').style.display = "block";
+                document.getElementById('next-round').onclick = function() {
+                    roundFour();
+                }
+                $('#audio').html('');
+            } else {
+                document.getElementById('France').style.fill = "black";
+                document.getElementById('pop-up-text').innerHTML = "<p>Wrong Answer</p>";
+                document.getElementById('pop-up').style.display = "block";
+                document.getElementById('next-round').onclick = function() {
+                    roundFour();
+                }
+                $('#audio').html('');
+            }
+        })
+    }
 }
 
 // Round Four
@@ -118,34 +145,25 @@ function roundFour() {
     
     let country = document.getElementsByClassName("country");
     
-    for (let i=0; i < country.length; i++) {
-        country[i].addEventListener('click', function(e) {
-            if(country[i].id == 'Germany') {
-                document.getElementById('Germany').style.fill = "black";
-                document.getElementById('pop-up-text').innerHTML = "<p>Correct Answer</p>";
-                document.getElementById('pop-up').style.display = "block";
-                document.getElementById('next-round').onclick = function() {
-                    roundFive();
-                }
-                $('#audio').html('');
-            } else {
-                document.getElementById('Germany').style.fill = "black";
-                document.getElementById('pop-up-text').innerHTML = "<p>Wrong Answer</p>";
-                document.getElementById('pop-up').style.display = "block";
-                document.getElementById('next-round').onclick = function() {
-                    roundFive();
-                }
-                $('#audio').html('');
-            }
-        })
-    }
-}
-
-
 document.getElementById('pause').onclick = function() {
     $('#audio').html('');
 }
 
+let country = document.getElementsByClassName("country")
+
+for (let i=0; i < country.length; i++) {
+    country[i].addEventListener('click', function(e) {
+        if(country[i].id == 'Indonesia') {
+            alert('YAY');
+            $('#audio').html('');
+        } else {
+            alert('WRONG');
+            $('#audio').html('');
+        }
+    })
+}
+
+// Map Information
 var tooltipSpan = document.getElementById('details-box');
 var map = document.querySelectorAll('[data-name]');
 document.addEventListener('mouseover', function (e) {
