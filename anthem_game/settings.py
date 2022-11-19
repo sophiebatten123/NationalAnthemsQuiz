cd3eb86df9120bd2465d1a6e230a7238a160b55b
LauraMayock <mcfadden.laura@yahoo.com>
2022-11-19 15:14:28 +0000

    update footer

--- anthem_game/settings.py
+++ anthem_game/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
+    'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
