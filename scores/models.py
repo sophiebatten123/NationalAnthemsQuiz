06efe4cf57bf20785bd5cfa5037cc3b0ab6d583f
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-11-19 18:11:15 +0000

    add score model, admin and form

--- scores/models.py
+++ scores/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Score(models.Model):
+    """ A model to keep user scores"""
+
+    name = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_score')
+    score = models.IntegerField()
+    created_on = models.DateTimeField(auto_now=True)
+    position = models.IntegerField()
+
+    class Meta:
+        """ order by score """
+        ordering = ['score']
+
+    def __str__(self):
+        return f"{self.name}"
