b195adb2e77e0fc45e8729a2126de1d3661cee26
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-11-20 20:33:51 +0000

    Removed scores urls from app

--- anthem_game/urls.py
+++ anthem_game/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('game.urls')),
-    path('', include('scores.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
