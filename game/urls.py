13d6c7e4d6bfe8e7d1292cbaa6f5e6d36f639bc5
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-11-20 09:25:22 +0000

    add style to login,out,register pages

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.game_view, name='game_view'),
+    path('', views.game_view, name='index'),
     path('add_score/', views.add_score, name='add_score'),
     ]
