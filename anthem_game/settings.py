33f51da34ad8662b81753106828c4599546fc573
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-11-24 13:28:34 +0000

    Migrate project to Render

--- anthem_game/settings.py
+++ anthem_game/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['anthem-game.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
